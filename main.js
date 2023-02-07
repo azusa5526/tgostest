2769ab64c1fd6864e20789adb58721ec740814b9
AndrewYao <azusa5526@gmail.com>
2023-02-07 10:50:57 +0800

    feat: 增加 LineString、Polygon

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ import mygeodata from "./station.json";
 const TGMap = document.getElementById("TGMap");
 let pMap = null;
 let infoWindow = null;
-let markers = [
+const markers = [
   { point: { lat: 24.783076, lng: 120.999146 }, content: "ABCDE" },
-  { point: { lat: 24.283076, lng: 120.199146 }, content: `<input class="owen" type="text">` },
+  {
+    point: { lat: 24.283076, lng: 120.199146 },
+    content: `<input class="owen" type="text">`,
+  },
+];
+const lines = [
+  [
+    { lat: 24.983076, lng: 120.999146 },
+    { lat: 24.683076, lng: 120.199146 },
+    { lat: 24.183076, lng: 120.399146 },
+  ],
+  [
+    { lat: 24.183076, lng: 121.999146 },
+    { lat: 24.583076, lng: 120.299146 },
+    { lat: 24.283076, lng: 120.799146 },
+  ],
+];
+const polygons = [
+  [
+    { lat: 23.983076, lng: 120.999146 },
+    { lat: 23.683076, lng: 120.199146 },
+    { lat: 23.183076, lng: 120.399146 },
+    { lat: 23.283076, lng: 120.799146 },
+  ],
+  [
+    { lat: 24.183076, lng: 122.999146 },
+    { lat: 23.583076, lng: 122.299146 },
+    { lat: 23.283076, lng: 122.799146 },
+  ],
 ];
 
 async function initTGMap() {
@@ -48,11 +76,11 @@ function getMapOptions() {
   return mapOptions;
 }
 
-async function loadMarkers() {
+function loadMarkers() {
   const imgUrl = "./marker.svg"; //標記點圖示來源
 
-  for (let marker of markers) {
-    const point = new window.TGOS.TGPoint(marker.point.lng, marker.point.lat);
+  const pTGMarkers = markers.map((marker) => {
+    const tgosPoint = new window.TGOS.TGPoint(marker.point.lng, marker.point.lat);
     //設定標記點圖片及尺寸大小
     const markerImg = new window.TGOS.TGImage(
       imgUrl,
@@ -61,18 +89,85 @@ async function loadMarkers() {
       new window.TGOS.TGPoint(10, 33)
     );
 
-    const pTGMarker = new window.TGOS.TGMarker(pMap, point, "", markerImg); //建立機關單位標記點
+    const pTGMarker = new window.TGOS.TGMarker(pMap, tgosPoint, "", markerImg); //建立機關單位標記點
 
-    window.TGOS.TGEvent.addListener(pTGMarker, "click", () => {
-      openInfoWindow(point, marker.content);
+    window.TGOS.TGEvent.addListener(pTGMarker, "click", (args) => {
+      openInfoWindow(
+        tgosPoint,
+        marker.content,
+        args.target instanceof TGOS.TGMarker
+      );
     });
-  }
+
+    return pTGMarker;
+  });
+
+  console.log("loadMarkers", pTGMarkers);
 }
 
-async function initInfoWindow() {
+function loadPolylines() {
+  const paths = lines.map((line) => {
+    return line.map((point) => new window.TGOS.TGPoint(point.lng, point.lat));
+  });
+
+  const tgosLineStrings = paths.map(
+    (lineString) => new window.TGOS.TGLineString(lineString)
+  );
+
+  const pTGLines = tgosLineStrings.map((tgosLineString) => {
+    const pTGLine = new window.TGOS.TGLine(pMap, tgosLineString, {
+      strokeWeight: 10,
+    });
+
+    window.TGOS.TGEvent.addListener(pTGLine, "click", (args) => {
+      console.log("args", args);
+      openInfoWindow(args.point, "LineString123");
+    });
+
+    return pTGLine;
+  });
+
+  console.log("loadPolylines", pTGLines);
+}
+
+function loadPolygons() {
+  const paths = polygons.map((polygon) => {
+    return polygon.map(
+      (point) => new window.TGOS.TGPoint(point.lng, point.lat)
+    );
+  });
+
+  const tgosLineStrings = paths.map(
+    (lineString) => new window.TGOS.TGLineString(lineString)
+  );
+
+  const tgosLineRings = tgosLineStrings.map((tgosLineString) => [
+    new window.TGOS.TGLinearRing(tgosLineString),
+  ]);
+
+  const tgosPolygons = tgosLineRings.map(
+    (tgosLineRing) => new window.TGOS.TGPolygon(tgosLineRing)
+  );
+
+  const pTGFills = tgosPolygons.map((tgosPolygon) => {
+    const pTGFill = new window.TGOS.TGFill(pMap, tgosPolygon, {
+      strokeWeight: 10,
+    });
+
+    window.TGOS.TGEvent.addListener(pTGFill, "click", (args) => {
+      console.log("args", args);
+      openInfoWindow(args.point, "Polygon123");
+    });
+
+    return pTGFill;
+  });
+
+  console.log("loadPolygons", pTGFills);
+}
+
+function initInfoWindow() {
   let infoWindowOptions = {
     maxWidth: 300,
-    pixelOffset: new window.TGOS.TGSize(5, -30),
     zIndex: 99,
   };
 
@@ -80,7 +175,9 @@ async function initInfoWindow() {
   infoWindow.setOptions(infoWindowOptions);
 }
 
-function openInfoWindow(point, content) {
+function openInfoWindow(point, content, offsetState) {
+  if (offsetState)
+    infoWindow.setOptions({ pixelOffset: new window.TGOS.TGSize(0, -32) });
   infoWindow.setPosition(point);
   infoWindow.setContent(content);
   infoWindow.open(pMap);
@@ -94,6 +191,8 @@ function openInfoWindow(point, content) {
 (async () => {
   pMap = await initTGMap();
   // await loadGeoJson();
-  await loadMarkers();
-  await initInfoWindow();
+  loadMarkers();
+  loadPolylines();
+  loadPolygons();
+  initInfoWindow();
 })();
